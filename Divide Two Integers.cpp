53d861bbf94d979a9ea4c622560367d45f2e7131
VasilyevaAS <60739232+VasilyevaAS@users.noreply.github.com>
2020-03-08 20:46:05 +0300

    Create Divide Two Integers.cpp

---
+++ Divide Two Integers.cpp
@@ -0,0 +1,27 @@
+//https://leetcode.com/problems/divide-two-integers/
+
+
+class Solution {
+    public:
+    int divide(int dividend, int divisor) {
+        int sign = (dividend < 0) ^ (divisor < 0) ? -1 : 1;
+        long long dvd = dividend;
+        long long dvs = divisor;
+        
+        if (dvd == INT32_MIN && dvs == -1){
+            return INT_MAX;
+        }
+        dvd = abs(dvd);
+        dvs = abs(dvs);
+        
+        long long res =0;
+        
+        for (int i = 31; i >= 0; --i){
+            if (dvd >= (dvs << i)){
+                dvd -= (dvs << i);
+                res += (1 << i);
+            }
+        }
+        return sign * res;
+    }
+};
