302991e2b56d221a8c0b8d0812fc1d64714fc5f7
VasilyevaAS <60739232+VasilyevaAS@users.noreply.github.com>
2020-03-31 16:56:03 +0300

    Create Plus One.java

---
+++ Plus One.java
@@ -0,0 +1,24 @@
+//https://leetcode.com/problems/plus-one/
+
+
+class Solution {
+    public int[] plusOne(int[] digits) {
+        int index = digits.length - 1;
+        
+        while(digits[index] == 9){
+            digits[index] = 0;
+            index--;
+            
+            if(index < 0){
+                digits = new int[digits.length + 1];
+                digits[0] = 1;
+                break;
+            }
+        }
+        
+        if (index >= 0){
+            digits[index]++;
+        }
+        return digits;
+    }
+}
