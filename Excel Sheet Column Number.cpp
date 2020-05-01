fc996b96f19b00c99f80170ca274c80393b39a4c
VasilyevaAS <60739232+VasilyevaAS@users.noreply.github.com>
2020-05-01 18:28:35 +0300

    Create Excel Sheet Column Number.cpp

---
+++ Excel Sheet Column Number.cpp
@@ -0,0 +1,14 @@
+//https://leetcode.com/problems/excel-sheet-column-number/
+
+
+
+class Solution {
+public:
+    int titleToNumber(const string &s) {
+        int ans = 0;
+        for (char c : s){
+            ans = ans * 26 + (c - 'A' + 1);
+        }
+        return ans;
+    }
+};
