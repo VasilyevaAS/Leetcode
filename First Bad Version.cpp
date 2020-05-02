ebfaf282ae07e9d8340bd15874975512f76f5233
VasilyevaAS <60739232+VasilyevaAS@users.noreply.github.com>
2020-05-02 21:18:27 +0300

    Create First Bad Version.cpp

---
+++ First Bad Version.cpp
@@ -0,0 +1,23 @@
+//https://leetcode.com/problems/first-bad-version/
+
+
+// The API isBadVersion is defined for you.
+bool isBadVersion(int version);
+
+class Solution {
+public:
+    int firstBadVersion(int n) {
+        int good = 0;
+        int bad = n;
+        while (bad > good + 1){
+            int mid = good +((bad - good) >> 1);
+            if (isBadVersion(mid)){
+                bad = mid;
+            } else {
+                good = mid;
+            }
+        }
+        assert(bad == good + 1);
+        return bad;
+    }
+};
