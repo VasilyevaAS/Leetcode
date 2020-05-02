305c633c2473aca09a6117bb88cf44b9d01eeccb
VasilyevaAS <60739232+VasilyevaAS@users.noreply.github.com>
2020-05-02 21:49:08 +0300

    Create Word Pattern.cpp

---
+++ Word Pattern.cpp
@@ -0,0 +1,23 @@
+//https://leetcode.com/problems/word-pattern/
+
+
+class Solution {
+public:
+    bool wordPattern(string pattern, string str) {
+        unordered_map<char, string> m;
+        istringstream in(str);
+        int i = 0, n = pattern.size();
+        for (string word; in >> word; ++i) {
+            if (i >= n) continue;
+            if (m.count(pattern[i])) {
+                if (m[pattern[i]] != word) return false;
+            } else {
+                for (auto a : m) {
+                    if (a.second == word) return false;
+                }
+                m[pattern[i]] = word;
+            }
+        }
+        return i == n;
+    }
+};
