5781c861f193e2a599f0641c612d4f9f04780052
VasilyevaAS <60739232+VasilyevaAS@users.noreply.github.com>
2020-05-02 21:14:28 +0300

    Create Factorial Trailing Zeroes.cpp

---
+++ Factorial Trailing Zeroes.cpp
@@ -0,0 +1,14 @@
+//https://leetcode.com/problems/factorial-trailing-zeroes/
+
+
+class Solution {
+public:
+    int trailingZeroes(int n) {
+        int sum = 0;
+        while (n > 0) {
+            n /= 5;
+            sum += n;
+        }
+        return sum;
+    }
+};
