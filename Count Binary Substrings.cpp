c83009392e4ceea5686dc8d844e24962eb7e6bba
VasilyevaAS <60739232+VasilyevaAS@users.noreply.github.com>
2020-05-03 12:57:49 +0300

    Create Count Binary Substrings.cpp

---
+++ Count Binary Substrings.cpp
@@ -0,0 +1,23 @@
+//https://leetcode.com/problems/count-binary-substrings/
+
+class Solution {
+public:
+    int countBinarySubstrings(const string &s) {
+        int count = 0;
+        int prevSize = 0;
+        int curSize = 0;
+        int curC = '$';
+        for (char c : s){
+            if (c == curC){
+                curSize++;
+            } else {
+                count += min(prevSize, curSize);
+                prevSize = curSize;
+                curC = c;
+                curSize = 1;
+            }
+        }
+        count += min(prevSize, curSize);
+        return count;
+    }
+};
