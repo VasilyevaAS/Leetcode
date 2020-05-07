3503d4053db01eb96738ea57f813a0369d967268
VasilyevaAS <60739232+VasilyevaAS@users.noreply.github.com>
2020-05-07 17:41:57 +0300

    Create Distribute Candies to People.cpp

---
+++ Distribute Candies to People.cpp
@@ -0,0 +1,23 @@
+//https://leetcode.com/problems/distribute-candies-to-people/
+
+class Solution {
+public:
+    vector<int> distributeCandies(int candies, int num_people) {
+        vector<int> res(num_people, 0);
+        int cur = 1;
+        while (candies > 0) {
+            for (int i = 0; i < num_people; i++) {
+                if (candies >= cur) {
+                    res[i] += cur;
+                    candies -= cur;
+                } else {
+                    res[i] += candies;
+                    candies = 0;
+                    break;
+                }
+                cur++;
+            }
+        }
+        return res;
+    }
+};
