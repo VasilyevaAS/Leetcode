209aa0f8811d9eb0cdc4834dd6a53f8211dd5acd
VasilyevaAS <60739232+VasilyevaAS@users.noreply.github.com>
2020-05-07 23:55:53 +0300

    Create Sum of Two Integers.cpp

---
+++ Sum of Two Integers.cpp
@@ -0,0 +1,10 @@
+//https://leetcode.com/problems/sum-of-two-integers/
+
+class Solution {
+public:
+    int getSum(int a, int b) {
+      if(b == 0) return a;
+      int sum = a ^ b, carry = uint(a&b)<<1;
+      return getSum(sum,carry);
+    }
+};
