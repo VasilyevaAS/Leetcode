a0177119a2103ca1e7ae047e812c68b3238cb1b0
VasilyevaAS <60739232+VasilyevaAS@users.noreply.github.com>
2020-05-07 17:32:44 +0300

    Create Binary Prefix Divisible By 5.cpp

---
+++ Binary Prefix Divisible By 5.cpp
@@ -0,0 +1,14 @@
+//https://leetcode.com/problems/binary-prefix-divisible-by-5/
+
+class Solution {
+public:
+    vector<bool> prefixesDivBy5(vector<int>& A) {
+        vector<bool> result;
+        int x = 0;
+        for (const auto y: A) {
+            x = ((x << 1) + y) % 5;
+            result.push_back(x % 5 == 0);
+        }
+        return result;
+    }
+};
