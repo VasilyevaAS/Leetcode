354fec71baf73540de4ae26c9b2c0db2b3dadf7e
VasilyevaAS <60739232+VasilyevaAS@users.noreply.github.com>
2020-05-07 23:47:26 +0300

    Create Valid Perfect Square.cpp

---
+++ Valid Perfect Square.cpp
@@ -0,0 +1,21 @@
+//https://leetcode.com/problems/valid-perfect-square/
+
+class Solution{
+public:    
+    bool isPerfectSquare(int num) {        
+        if(num < 1) 
+            return false;        
+        if(num == 1) 
+            return true;        
+        int left = 0, right = num/2;        
+        while(left <= right){            
+            long mid = (left+right)/2;            
+            long val = mid*mid;            
+            if(val == num) 
+                return true;            
+            else if(val > num) right = mid-1;            
+            else left = mid+1;        
+        }        
+        return false;    
+    }
+};
