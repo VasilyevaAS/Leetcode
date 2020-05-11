624b4a1095c52d530de232c4a3b6ed61d9a60138
VasilyevaAS <60739232+VasilyevaAS@users.noreply.github.com>
2020-05-11 23:41:36 +0300

    Create Range Addition II.cpp

---
+++ Range Addition II.cpp
@@ -0,0 +1,15 @@
+//https://leetcode.com/problems/range-addition-ii/
+
+
+class Solution {
+public:
+    int maxCount(int m, int n, vector<vector<int>> &ops) {
+        int resultM = m;
+        int resultN = n;
+        for (const vector<int> &op : ops) {
+            resultM = min(resultM, op[0]);
+            resultN = min(resultN, op[1]);
+        }
+        return resultM * resultN;
+    }
+};
