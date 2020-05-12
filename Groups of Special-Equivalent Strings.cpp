e2b8448d17db61b28d16c99386afb70dca3c0038
VasilyevaAS <60739232+VasilyevaAS@users.noreply.github.com>
2020-05-12 10:47:56 +0300

    Create Groups of Special-Equivalent Strings.cpp

---
+++ Groups of Special-Equivalent Strings.cpp
@@ -0,0 +1,22 @@
+//https://leetcode.com/problems/groups-of-special-equivalent-strings/
+
+class Solution {
+public:
+  int numSpecialEquivGroups(vector<string>& A) {
+    unordered_set<string> s;
+    for (const string& a : A) {
+      string odd;
+      string even;
+      for (int i = 0; i < a.size(); ++i) {
+        if (i % 2)
+          odd += a[i];
+        else
+          even += a[i];
+      }
+      sort(begin(odd), end(odd));
+      sort(begin(even), end(even));
+      s.insert(odd + even);
+    }
+    return s.size();
+  }
+};
